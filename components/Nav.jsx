49865b60a54c87689cbf24de99592273f19c6124
Sxzar <contact.sxzar@gmail.com>
2023-07-13 18:35:47 +0300

    added the signed in nav

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,11 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 const Nav = () => {
+	{
+		/* Mock for the user log in status */
+	}
+	const isUserLoggedIn = true;
+
 	return (
 		<nav className="flex-between w-full mb-16 pt-3">
 			<Link href="/" className="flex gap-2 flex-center">
@@ -18,6 +23,36 @@ const Nav = () => {
 				/>
 				<p className="logo_text">Promptopia</p>
 			</Link>
+
+			{/* Desktop Nav */}
+			<div className="sm:flex hidden">
+				{isUserLoggedIn ? (
+					<div className="flex gap-3 md:gap-5">
+						<Link href="/create-prompt" className="black_btn">
+							Create Post
+						</Link>
+						<button
+							type="button"
+							onClick={signOut}
+							className="outline_btn"
+						>
+							Sign Out
+						</button>
+
+						<Link href="/profile">
+							<Image
+								src="/assets/images/logo.svg"
+								width={37}
+								height={37}
+								className="rounded-full"
+								alt="profile"
+							/>
+						</Link>
+					</div>
+				) : (
+					<></>
+				)}
+			</div>
 		</nav>
 	);
 };
