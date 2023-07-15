f08d7772ef848b69e4927b2f57483dbca6b76663
Sxzar <contact.sxzar@gmail.com>
2023-07-15 20:29:16 +0300

    display prompts on homepage

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,5 @@
 import NextAuth from "next-auth";
-import {
-	default as Google,
-	default as GoogleProvider,
-} from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 import User from "@models/user";
 import { connectToDB } from "@utils/database";
@@ -16,30 +13,33 @@ const handler = NextAuth({
 	],
 	callbacks: {
 		async session({ session }) {
+			// store the user id from MongoDB to session
 			const sessionUser = await User.findOne({
 				email: session.user.email,
 			});
 			session.user.id = sessionUser._id.toString();
+
 			return session;
 		},
-		async signIn({ profile }) {
+		async signIn({ account, profile, user, credentials }) {
 			try {
 				await connectToDB();
 
-				// check if a user already exists with this email
+				// check if user already exists
 				const userExists = await User.findOne({ email: profile.email });
 
-				// if not, create a new user
+				// if not, create a new document and save user in MongoDB
 				if (!userExists) {
 					await User.create({
 						email: profile.email,
 						username: profile.name.replace(" ", "").toLowerCase(),
-						image: profile.picture,
+						image: profile.picture.toString(),
 					});
 				}
+
 				return true;
 			} catch (error) {
-				console.log("Error: ", error);
+				console.log("Error checking if user exists: ", error.message);
 				return false;
 			}
 		},
